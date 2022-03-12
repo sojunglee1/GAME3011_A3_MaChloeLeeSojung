6d86f588a8cea24c4e05ca6e79ad5f2b62878bca
Sojung Lee <sojung.lee2@georgebrown.ca>
2022-03-11 23:19:28 -0500

    Added basic art + created grid generator

---
+++ GAME3011_A3/Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [Range(8, 16)]
+    public int boardWidth, boardHeight;
+
+    public GameObject gameBoard;
+
+    public GameObject slot;
+    public List<Slot> slotList;
+
+    public GameObject tile;
+    public List<Image> tileSprites;
+
+    [System.Obsolete]
+    private void Start()
+    {
+        CreateGameBoard();
+        Random.seed = 42;
+
+        
+
+    }
+
+    public void CreateGameBoard()
+    {
+        float centerX = gameBoard.GetComponent<RectTransform>().rect.width / 2;
+        float centerY = gameBoard.GetComponent<RectTransform>().rect.height / 2;
+
+        float itemWidth = slot.GetComponent<RectTransform>().rect.width;
+        float itemHeight = slot.GetComponent<RectTransform>().rect.height;
+
+        for (float i = -boardWidth/2; i <= boardWidth / 2; i++)
+        {
+            for (int j = -boardHeight/2; j <= boardHeight/2; j++)
+            {
+                Vector3 pos = new Vector3(centerX - (i * itemWidth), centerY - (j * itemHeight), 0);
+                var newSlot = Instantiate(slot, pos, Quaternion.identity, gameBoard.transform);
+
+                slotList.Add(newSlot.GetComponent<Slot>());
+            }
+        }
+    }
+
+    public void RandomItems()
+    {
+        int randomizeItems = Random.Range(0, tileSprites.Count);
+        for(int i = 0; i < slotList.Count; i++)
+        {
+               
+        }
+    }
+}
---
+++ GAME3011_A3/Assets/Scripts/Tile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Tile : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public bool selected;
+    public Image tileSprite;
+    CanvasGroup canvasGroup;
+
+    private void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        selected = true;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        selected = false;
+        canvasGroup.blocksRaycasts = true;
+    }
+}
