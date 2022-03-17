cd96f075e703402e653247d1d0afb30feba78d3d
Sojung Lee <sojung.lee2@georgebrown.ca>
2022-03-16 20:11:44 -0400

    Able to match 3 tiles together (temp: fading)

--- GAME3011_A3_MaChloe_LeeSojung/Assets/GameManager.cs
+++ GAME3011_A3_MaChloe_LeeSojung/Assets/GameManager.cs
@@ -4,34 +4,50 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [Range(8, 16)]
+    [Range(2, 16)]
     public int boardWidth;
-    [Range(8, 16)]
+    [Range(2, 16)]
     public int boardHeight;
 
-    public GameObject tile;
+    public Tile tile;
     public GameObject gameBoard;
-
-    public GameObject[,] tiles;
+    public Dictionary<Vector2, Tile> tiles;
 
     void Start()
     {
-        tiles = new GameObject[boardWidth + 1, boardHeight + 1];
-        float boundsX = tile.GetComponent<BoxCollider2D>().size.x;
+        tiles = new Dictionary<Vector2, Tile>(boardWidth * boardHeight);
+
         for (int x = 0; x <= boardWidth; x++)
         {
             for (int y = 0; y <= boardHeight; y++)
             {
                 var newTile = Instantiate(tile, new Vector3(x - (float)boardWidth/2, y - (float)boardHeight/2, 0), Quaternion.identity, gameBoard.transform);
-                tiles[x, y] = newTile;
-
+                newTile.DrawTile();
+                newTile.ID = new Vector2(newTile.transform.position.x, newTile.transform.position.y);
+                tiles[newTile.ID] = newTile;
             }
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FindMatch()
     {
-        
+
+        var hitData = Physics2D.Raycast(tiles[new Vector2(0,0)].topRay.origin, tiles[Vector2.zero].topRay.direction, 2.0f);
+        var hitDataCollider = hitData.collider;
+        if (hitData)
+        {
+            print(hitData.transform.position);
+            if (tiles[Vector2.zero].sprite.Equals(hitDataCollider.GetComponent<Tile>().sprite))
+            {
+                print("match!");
+                print(hitData.collider.gameObject.GetComponent<Tile>().ID);
+            }
+            else
+            {
+                print("no match!");
+            }
+        }
+        else print("didn't hit!");
     }
+
 }
--- GAME3011_A3_MaChloe_LeeSojung/Assets/Tile.cs
+++ GAME3011_A3_MaChloe_LeeSojung/Assets/Tile.cs
@@ -4,24 +4,92 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer;
     public List<Sprite> possibleTiles;
+    public Vector2 ID { get; set; }
+
+    public Ray2D topRay { get; set; }
+    public Ray2D bottomRay { get; set; }
+    public Ray2D leftRay { get; set; }
+    public Ray2D rightRay { get; set; }
+
+    public List<RaycastHit2D> rays;
+
+    public Animator animator;
+
+    public Sprite sprite
+    {
+        get { return GetComponent<SpriteRenderer>().sprite; }
+        set { GetComponent<SpriteRenderer>().sprite = value; }
+    }
 
     private void Start()
     {
-        int random = Random.Range(0, possibleTiles.Count);
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.sprite = possibleTiles[random];
+        animator = GetComponent<Animator>();
+        topRay = new Ray2D(transform.position, Vector3.up);
+        bottomRay = new Ray2D(transform.position, -Vector3.up);
+        leftRay = new Ray2D(transform.position, -Vector3.right);
+        rightRay = new Ray2D(transform.position, Vector3.right);
+    }
+
+    private void Update()
+    {
+        Matched();
+    }
+
+    public void Matched()
+    {
+        //CheckAdjacentTiles(bottomRay);
+        CheckAdjacentTiles(topRay);
+        //CheckAdjacentTiles(leftRay);
+        CheckAdjacentTiles(rightRay);
+
+        //if (MatchTiles(bottomRay) && MatchTiles(topRay))
+        //{
+        //    print("matched 3");
+        //}
+        //else if (MatchTiles(leftRay) && MatchTiles(rightRay))
+        //{
+        //    print("matched 3");
+        //}
+    }
+
+    public void CheckAdjacentTiles(Ray2D ray)
+    {
+        var hitData = Physics2D.Raycast(ray.origin, ray.direction, 1.0f);
+        var hitData2 = Physics2D.Raycast(ray.origin, -ray.direction, 1.0f);
+        if (hitData && hitData2)
+        {
+            if (hitData.collider.gameObject.GetComponent<Tile>().sprite.Equals(this.sprite) &&
+                hitData2.collider.gameObject.GetComponent<Tile>().sprite.Equals(this.sprite))
+            {
+                animator.Play("Fade");
+                hitData.collider.gameObject.GetComponent<Tile>().animator.Play("Fade");
+                hitData2.collider.gameObject.GetComponent<Tile>().animator.Play("Fade");
+            }
+        }
+    }
+
+    public bool MatchTiles(Ray2D ray)
+    {
+        bool matched = false;
+        var hitData = Physics2D.Raycast(ray.origin, ray.direction, 1.0f);
+        if (hitData)
+        {
+            if (hitData.collider.gameObject.GetComponent<Tile>().sprite.Equals(this.sprite))
+                matched = true;
+            else matched = false;
+        }
+
+        return matched;
+    }
+
+    public void DrawTile()
+    {
+        sprite = possibleTiles[Random.Range(0, possibleTiles.Count)];
     }
 
     public void OnDrawGizmos()
     {
-        Ray2D topRay = new Ray2D(transform.position, Vector3.up);
-        Ray2D bottomRay = new Ray2D(transform.position, -Vector3.up);
-        Ray2D leftRay = new Ray2D(transform.position, -Vector3.right);
-        Ray2D rightRay = new Ray2D(transform.position, Vector3.right);
-
-
         Debug.DrawRay(topRay.origin, topRay.direction, Color.green);
         Debug.DrawRay(bottomRay.origin, bottomRay.direction, Color.green);
         Debug.DrawRay(leftRay.origin, leftRay.direction, Color.green);
